2e57703161ad329e2a4078d1c22592895db93290
Luke Olali <mugapedia@gmail.com>
2021-07-29 00:16:32 +0300

    fixes an error with regards to the declaration of the edges property

--- graphs/graph_ds.java
+++ graphs/graph_ds.java
@@ -6,11 +6,13 @@ import java.util.Arrays;
 
 class Graph {
 
+  Map<Integer, ArrayList<Integer>> edges;
+
   /**
    * Constructs an empty graph
    */
   public Graph() {
-    Map<Integer, ArrayList<Integer>> edges;
+    
   }
 
   /**
