857b2843328782c2cbf016abee290c489c608a70
Luke Olali <mugapedia@gmail.com>
2021-07-30 22:44:18 +0300

    defines method that adds a vertex to the graph

--- graphs/all_pairs_shortest_path.java
+++ graphs/all_pairs_shortest_path.java
@@ -32,4 +32,15 @@ class DiGraph {
       }
     }
   }
+
+  /**
+   * Add a vertex
+   * @param int vertex - A vertex on the graph
+   */
+  public void addVertex(int vertex) {
+    if(!edges.containsKey(vertex)) {
+      ArrayList<HashMap<Integer, Integer>> adjacencyList = new ArrayList<HashMap<Integer, Integer>>();
+      edges.put(vertex, adjacencyList);
+    }
+  }
 }
