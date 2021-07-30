d17ecbff122e8f11f903f994cd27ce7734857e1c
Luke Olali <mugapedia@gmail.com>
2021-07-30 22:38:55 +0300

    initializes the graph without any edges

--- graphs/all_pairs_shortest_path.java
+++ graphs/all_pairs_shortest_path.java
@@ -2,7 +2,10 @@ class DiGraph {
 
   Map<Integer, ArrayList<HashMap<Integer, Integer>>> edges;
 
+  /**
+   * Constructs an empty graph
+   */
   public DiGraph() {
-    
+    edges = new HashMap<Integer, ArrayList<HashMap<Integer, Integer>>>();
   }
 }
