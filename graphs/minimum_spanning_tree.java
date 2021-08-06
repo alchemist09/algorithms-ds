2399efa43242c9a10695e864aba1be7bb96e950c
Luke Olali <mugapedia@gmail.com>
2021-08-06 23:40:58 +0300

    defines the format for storing weighted edges of the graph

--- graphs/minimum_spanning_tree.java
+++ graphs/minimum_spanning_tree.java
@@ -1,6 +1,12 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
 class Graph2 {
 
-  public Graph2() {
+  Map<Integer, HashMap<Integer, Integer>> edges;
 
+  public Graph2() {
+    edges = new HashMap<Integer, HashMap<Integer, Integer>>();
   }
 }
