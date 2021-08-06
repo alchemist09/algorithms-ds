a5559c8154cb2f6e11c55b31ded90e046d13830d
Luke Olali <mugapedia@gmail.com>
2021-08-06 23:45:57 +0300

    adds docstrings for the constructors

--- graphs/minimum_spanning_tree.java
+++ graphs/minimum_spanning_tree.java
@@ -6,10 +6,17 @@ class Graph2 {
 
   Map<Integer, HashMap<Integer, Integer>> edges;
 
+  /**
+   * Constructs an empty graph
+   */
   public Graph2() {
     edges = new HashMap<Integer, HashMap<Integer, Integer>>();
   }
 
+  /**
+   * Constructs a graph with weighted edges
+   * @param edgesMap - HashMap containing connected edge info 
+   */
   public Graph2(Map<Integer, HashMap<Integer, Integer>> edgesMap) {
     edges = new HashMap<Integer, HashMap<Integer, Integer>>();
     this.loadGraph(edgesMap);
