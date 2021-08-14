1a6b22cf7e8190c04065e9dbe1d1062da5834b2e
Luke Olali <mugapedia@gmail.com>
2021-08-14 23:58:58 +0300

    defines method to add a vertex to the graph

--- graphs/dijkistra.java
+++ graphs/dijkistra.java
@@ -13,4 +13,15 @@ class DiGraph2 {
   public DiGraph2() {
     edges = new HashMap<Integer, ArrayList<HashMap<Integer, Integer>>>();
   }
+
+  /**
+   * Add a vertex
+   * @param int vertex - A vertex on the graph
+   */
+  public void addVertex(int vertex) {
+    if(!edges.containsKey(vertex)) {
+      ArrayList<HashMap<Integer, Integer>> adjacencyList = new ArrayList<HashMap<Integer, Integer>>();
+      edges.put(vertex, adjacencyList);
+    }
+  }
 }
