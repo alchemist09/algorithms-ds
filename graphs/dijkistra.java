71a0fbc290e1c338f4ec1b448e71ddd6b594bbb5
Luke Olali <mugapedia@gmail.com>
2021-08-14 23:49:54 +0300

    declares class that implements a directed graph

---
+++ graphs/dijkistra.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+
+class DiGraph {
+  Map<Integer, ArrayList<HashMap<Integer, Integer>>> edges;
+
+  public DiGraph() {
+    
+  }
+}
