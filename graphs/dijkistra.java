38c12b3b1a301c065728148656476bccb8a41af3
Luke Olali <mugapedia@gmail.com>
2021-08-20 23:40:05 +0300

    loads graph with edges from an adjacency list

--- graphs/dijkistra.java
+++ graphs/dijkistra.java
@@ -48,4 +48,20 @@ class DiGraph2 {
       adjacencyList.add(dst);
     }
   }
+
+  /**
+   * Populate Graph with List of Edges
+   * @param Map edges - The Adjacency list representation of the graph
+   */
+  public void loadGraph(Map<Integer, ArrayList<HashMap<Integer, Integer>>> edges) {
+    for(int vertex: edges.keySet()) {
+      this.addVertex(vertex);
+      for(Map<Integer, Integer> dest_wgt_pair : edges.get(vertex)) {
+        Integer destValue = (Integer)dest_wgt_pair.keySet().toArray()[0];
+        int dest = destValue.intValue();
+        int wgt = dest_wgt_pair.get(dest);
+        this.addEdge(vertex, dest, wgt);
+      }
+    }
+  }
 }
