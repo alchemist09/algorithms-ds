160ab4991532cde74b363bbfa134200033ba910c
Luke Olali <mugapedia@gmail.com>
2021-09-05 00:11:26 +0300

    defines constructor for NQueens class

--- backtracking/nqueens.java
+++ backtracking/nqueens.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 class NQueens {
   int count;
   int num_queens;
   Integer[] nTuple;
   List<Integer[]> solutions;
+
+  public NQueens(int no_of_queens) {
+    count = 0;
+    num_queens = no_of_queens;
+    nTuple = new Integer[no_of_queens];
+    Arrays.fill(nTuple, new Integer(-1));
+    solutions = new ArrayList<Integer[]>();
+    System.out.println("Constructor nTuple: " + Arrays.asList(nTuple) + "\r\n");
+  }
 }
