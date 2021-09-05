23246ef4fa441f8e728a47511e2439e34ce27187
Luke Olali <mugapedia@gmail.com>
2021-09-05 23:56:21 +0300

    determines whether queen can be placed on a particular cell

--- backtracking/nqueens.java
+++ backtracking/nqueens.java
@@ -16,4 +16,24 @@ class NQueens {
     solutions = new ArrayList<Integer[]>();
     System.out.println("Constructor nTuple: " + Arrays.asList(nTuple) + "\r\n");
   }
+
+  /**
+   * Determine whether queen can be plcaed on a particular cell
+   * @param k - number of rows on chess board
+   * @param thisCol - the current column under consideration
+   * @return True/False whether queen can be placed on a certain cell
+   */
+  public boolean canPlaceQueen(int k, int thisCol) {
+    // System.out.println("nTuple: " + Arrays.asList(nTuple));
+    for(int currRow=0; currRow < k; currRow++) {
+      // System.out.println("nTuple[currRow]: " + nTuple[currRow]);
+      // System.out.println("nTuple[currRow].intValue(): " + nTuple[currRow].intValue());
+      // System.out.println("\r\n");
+      if(nTuple[currRow].intValue() == thisCol ||
+         (Math.abs(nTuple[currRow].intValue() - thisCol) == Math.abs(k-currRow))) {
+          return false;
+        }
+    }
+    return true;
+  }
 }
