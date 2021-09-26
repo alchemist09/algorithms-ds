ae53067efd32b96004f2b8fcecc0c6116912c078
Luke Olali <mugapedia@gmail.com>
2021-09-26 23:13:51 +0300

    declares package to group dynamic programming files

--- dynamic_programming/min_cost_2_cities.java
+++ dynamic_programming/min_cost_2_cities.java
@@ -1,3 +1,5 @@
+package dynamic_programming;
+
 /* There are N stations in a route, starting from 0 to N-1. A train moves from 
 * station (0) to the last station (N-1) in only forward direction. The cost of
 * ticket between any two station is given. Find the minimum cost of travel between
