3e6c656a56a7278256b93eb30de707daaf430795
Luke Olali <mugapedia@gmail.com>
2021-10-02 23:43:41 +0300

    removes print statements used in testing the program

--- dynamic_programming/longest_palindromic_sum_substring.java
+++ dynamic_programming/longest_palindromic_sum_substring.java
@@ -1,6 +1,5 @@
 package dynamic_programming;
 
-import java.util.Arrays;
 
 class LongestPalindromicSumSubstring {
   public static void main(String[] args) {
@@ -12,13 +11,9 @@ class LongestPalindromicSumSubstring {
     int N = str.length();
     int[] sum = new int[N+1];
     
-    System.out.println(Arrays.toString(sum));
-    
     for(int i=1; i <= N; i++) {
       sum[i] = sum[i-1] + (str.charAt(i-1) - '0');
     }
-    
-    System.out.println(Arrays.toString(sum));
     
     int maxLen = 0;
     
@@ -27,11 +22,8 @@ class LongestPalindromicSumSubstring {
         // get length of substring 
         int substr = (k-j) + 1;
             
-        System.out.println("Substring: " + str.substring(j, k+1));
-            
         // get substring midpoint 
         int mid = j + (substr/2);
-        System.out.println("mid: " + mid);
             
         if((sum[mid] - sum[j] == sum[k+1] - sum[mid]) && substr > maxLen) {
           maxLen = substr;
