db6a81754d17f826447ce39db48d3c13e6d455b3
Luke Olali <mugapedia@gmail.com>
2021-10-06 23:19:39 +0300

    different DP algorithm for calculating the longest common substring

--- dynamic_programming/longest_common_substring.java
+++ dynamic_programming/longest_common_substring.java
@@ -40,5 +40,40 @@ class LongestCommonSubstring {
         }
     }
     return max;
-}
+  }
+
+  public static String lcs2(String a, String b) {
+    if(a == null || b == null) {
+        return "";
+    }
+    
+    int m = a.length();
+    int n = b.length();
+    
+    if(m == 0 || n == 0) {
+        return "";
+    }
+    
+    String res = "";
+    int maxLen = 0;
+    int[][] dp = new int[m][n];
+    
+    for(int i=0; i < m; i++) {
+        for(int j=0; j < n; j++) {
+            if(a.charAt(i) == b.charAt(j)) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                
+                if(dp[i][j] > maxLen) {
+                    maxLen = dp[i][j];
+                    res = a.substring(i-maxLen + 1, i+1);
+                }
+            }
+        }
+    }
+    return res;
+  }
 }
