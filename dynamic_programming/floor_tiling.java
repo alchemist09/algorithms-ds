e461a2614e27e8d1e2a7737257aecbb9dcdbbeb7
Luke Olali <mugapedia@gmail.com>
2021-10-13 23:58:57 +0300

    tests out the brute force method of tiling a floor

--- dynamic_programming/floor_tiling.java
+++ dynamic_programming/floor_tiling.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 class FloorTiling {
   public static void main(String[] args) {
-    
+    System.out.println("Floor Tiling Brute Force: " + floorTiling(10));
   }
 
   /**
