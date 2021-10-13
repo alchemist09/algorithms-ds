1717d654702605aec50ea1885382f3b60ef306fc
Luke Olali <mugapedia@gmail.com>
2021-10-13 23:57:24 +0300

    calculates number of unique ways to arrange tile in a 2xN floor size using brute force approach

---
+++ dynamic_programming/floor_tiling.java
@@ -0,0 +1,21 @@
+package dynamic_programming;
+
+import java.util.Arrays;
+
+class FloorTiling {
+  public static void main(String[] args) {
+    
+  }
+
+  /**
+   * Brute force approach of calculating the number of ways to tile a floor
+   * @param floor_length
+   * @return Count of unique ways to fit tiles in floor
+   */
+  public static long floorTiling(int floor_length) {
+    if(floor_length == 0) { return 0; }
+	  if(floor_length == 1) { return 1; }
+	  if(floor_length == 2) { return 2; }
+	  return floorTiling(floor_length - 1) + floorTiling(floor_length - 2);
+ }
+}
