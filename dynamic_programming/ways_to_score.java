af5f0c8ee9cb0fda84953f2d1651431307cb3cfc
Luke Olali <mugapedia@gmail.com>
2021-10-17 23:34:32 +0300

    calculates unique ways to reach target score using top-down memoization approach

--- dynamic_programming/ways_to_score.java
+++ dynamic_programming/ways_to_score.java
@@ -21,4 +21,25 @@ class WaysToScore {
     
     return waysToScoreBF(targetScore - 10) + waysToScoreBF(targetScore - 5) + waysToScoreBF(targetScore - 3);
   }
+
+  /**
+   * Calculate ways to reach target score using top-down memoization approach
+   * @param scores Memoization table to store solutions to pre-computed problems
+   * @param targetScore The score we're targeting
+   * @return Unique count of ways to reach target score
+   */
+  public static int waysToScoreMemo(int[] scores, int targetScore) {
+    if(targetScore < 0) { return 0; }
+    if(targetScore == 0) { return 1; }
+    
+    if(scores[targetScore] != 0) {
+        return scores[targetScore];
+    }
+    
+    scores[targetScore] = waysToScoreMemo(scores, targetScore - 10) 
+                        + waysToScoreMemo(scores, targetScore - 5) 
+                        + waysToScoreMemo(scores, targetScore - 3);
+   
+   return scores[targetScore];
+}
 }
