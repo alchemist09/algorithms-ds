b022535ef8e9fa5b3e806f4aaaed1bc2a07ed60d
Luke Olali <mugapedia@gmail.com>
2021-11-05 23:51:51 +0300

    tests out the brute force method

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -2,7 +2,8 @@ package dynamic_programming;
 
 class UniquePaths {
   public static void main(String[] args) {
-    
+    System.out.println(uniquePathsBF(2, 5));
+    System.out.println(uniquePathsBF(7, 3));
   }
 
   public static int uniquePathsBF(int m, int n) {
