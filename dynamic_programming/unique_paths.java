e956e0926afd00f6dc3572ff692afb50250e344a
Luke Olali <mugapedia@gmail.com>
2021-11-05 23:49:53 +0300

    fixes error caused my missing variable

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -8,6 +8,6 @@ class UniquePaths {
   public static int uniquePathsBF(int m, int n) {
     if(m == 0 && n == 0) { return 0; } // cell (0, 0)
     if(m == 0 || n == 0) { return 1; } // top-most column or left-most row
-    return uniquePathsBF(-1, n) + uniquePathsBF(m, n-1);
+    return uniquePathsBF(m-1, n) + uniquePathsBF(m, n-1);
   }
 }
