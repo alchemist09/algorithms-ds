86ba73ad2090c1154a2de564084a41d750ef5e59
Luke Olali <mugapedia@gmail.com>
2021-11-05 23:45:30 +0300

    finds the unique paths in a grid using brute force approach

---
+++ dynamic_programming/unique_paths.java
@@ -0,0 +1,13 @@
+package dynamic_programming;
+
+class UniquePaths {
+  public static void main(String[] args) {
+    
+  }
+
+  public static int uniquePathsBF(int m, int n) {
+    if(m == 0 && n == 0) { return 0; } // cell (0, 0)
+    if(m == 0 || n == 0) { return 1; } // top-most column or left-most row
+    return uniquePathsBF(-1, n) + uniquePathsBF(m, n-1);
+  }
+}
