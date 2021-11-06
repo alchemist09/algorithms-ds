6450ddebd99809229bea0eb997b2239c6e3b9091
Luke Olali <mugapedia@gmail.com>
2021-11-06 23:55:39 +0300

    recursive function that does the actual calculation of unique paths

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -28,5 +28,19 @@ class UniquePaths {
     System.out.println(Arrays.deepToString(T));
     
     return uniquePathsUtil(T, m, n);
-}
+  }
+
+  private static int uniquePathsUtil(int[][] cache, int m, int n) {
+    if(m == 1 || n == 1) {
+        return 1;
+    }
+    
+    if(cache[m-1][n-1] != -1) {
+        return cache[m-1][n-1];
+    }
+    
+    cache[m-1][n-1] = uniquePathsUtil(cache, m-1, n) + uniquePathsUtil(cache, m, n-1);
+    System.out.println(Arrays.deepToString(cache));
+    return cache[m-1][n-1];
+  }
 }
