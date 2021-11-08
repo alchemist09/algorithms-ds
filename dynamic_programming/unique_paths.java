b8931410ed4b151b4654d3ae79f2a56fa6355810
Luke Olali <mugapedia@gmail.com>
2021-11-08 16:26:21 +0300

    adds docstrings

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -17,12 +17,24 @@ class UniquePaths {
     System.out.println(uniquePathsBF(7, 3));
   }
 
+  /**
+   * Brute force approach of calculating the number of unique paths from top-left to bottom-right
+   * @param m Grid dimension along the Y-axis
+   * @param n Grid dimension along the X-axis
+   * @return The total number of unique ways to traverse the grid
+   */
   public static int uniquePathsBF(int m, int n) {
     if(m == 0 && n == 0) { return 0; } // cell (0, 0)
     if(m == 0 || n == 0) { return 1; } // top-most column or left-most row
     return uniquePathsBF(m-1, n) + uniquePathsBF(m, n-1);
   }
 
+  /**
+   * Top-Down memoization approach of calculating the number of unique paths from top-left to bottom-right
+   * @param m Grid dimension along the Y-axis
+   * @param n Grid dimension along the X-axis
+   * @return The total number of unique ways to traverse the grid
+   */
   public static int uniquePathsMemo(int m, int n) {
     if(m == 1 || n == 1) {
         return 1;
@@ -39,6 +51,13 @@ class UniquePaths {
     return uniquePathsUtil(T, m, n);
   }
 
+  /**
+   * Recursive utility function to help with the calculation of unique paths in grid
+   * @param cache Memoization table to store pre-computed values
+   * @param m Grid dimension along the Y-axis
+   * @param n Grid dimension along the X-axis
+   * @return The total number of unique ways to traverse the grid
+   */
   private static int uniquePathsUtil(int[][] cache, int m, int n) {
     if(m == 1 || n == 1) {
         return 1;
@@ -53,6 +72,12 @@ class UniquePaths {
     return cache[m-1][n-1];
   }
 
+  /**
+   * Bottom-up Dynamic Programming approach of calculating the number of unique paths from top-left to bottom-right
+   * @param m Grid dimension along the Y-axis
+   * @param n Grid dimension along the X-axis
+   * @return The total number of unique ways to traverse the grid
+   */
   public static int uniquePathsDP(int m, int n) {
     int[][] table = new int[m][n];
 
