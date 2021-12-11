9c078d727ac0533f69b2badda82681bdb5a81181
Luke Olali <mugapedia@gmail.com>
2021-12-11 23:48:39 +0300

    calculates the longest common subsequence between two string using brute force approach

---
+++ dynamic_programming/longest_common_subsequence.java
@@ -0,0 +1,25 @@
+package dynamic_programming;
+
+class LongestCommonSubsequence {
+  public static void main(String[] args) {
+    
+  }
+
+  public static int lcsRec(String m, String n) {
+    // if length of either input string is zero, there's no common subsequence
+    if(m.length() == 0 || n.length() == 0) {
+      return 0;
+    }
+
+    int m_len = m.length();
+    int n_len = n.length();
+  
+    // case where last character of either substring is same
+    if(m.substring(m_len-1) == n.substring(n_len-1)) {
+      return 1 + lcsRec(m.substring(0, m_len-1), n.substring(0, n_len-1));
+    } else {
+      // if ending characters are not same, return maximum of two recursive calls
+      return Math.max(lcsRec(m.substring(0, m_len-1), n), lcsRec(m, n.substring(0, n_len-1)));
+    }
+  }
+}
