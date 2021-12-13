f84f633099c0a77881a49a9244a704dfc1c89fec
Luke Olali <mugapedia@gmail.com>
2021-12-13 22:45:11 +0300

    defines bottom-up dynamic programming approach for solving the LCS problem

--- dynamic_programming/longest_common_subsequence.java
+++ dynamic_programming/longest_common_subsequence.java
@@ -68,4 +68,38 @@ class LongestCommonSubsequence {
         return cache[m-1][n-1];
     }
   }
+
+  public static int lcsDP1(String text1, String text2) {
+    if((text1 == null || text1.length() == 0) || (text2 == null || text2.length() == 0)) {
+        return 0;
+    }
+    
+    int m = text1.length();
+    int n = text2.length();
+    
+    int[][] T = new int[m+1][n+1];
+    
+    // fill top most row, case where text2 is empty
+    for(int j=1; j <= n; j++) {
+        T[0][j] = 0;
+    }
+    
+    // fill leftmost column, case where text1 is empty 
+    for(int i=1; i <= m; i++) {
+        T[i][0] = 0;
+    }
+    
+    // fill the rest of the cells
+    for(int i=1; i <= m; i++) {
+        for(int j=1; j <= n; j++) {
+            if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                T[i][j] = 1 + T[i-1][j-1];
+            } else {
+                T[i][j] = Math.max(T[i-1][j], T[i][j-1]);
+            }
+        }
+    }
+    
+    return T[m][n];
+  }
 }
