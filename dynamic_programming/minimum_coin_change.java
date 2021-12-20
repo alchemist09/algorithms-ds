a7a6739508119e4dc15ac06ef06b09b60e0de042
Luke Olali <mugapedia@gmail.com>
2021-12-20 23:42:20 +0300

    creates utility funciton that finds the minimum coins using recursion with memoization

--- dynamic_programming/minimum_coin_change.java
+++ dynamic_programming/minimum_coin_change.java
@@ -40,4 +40,38 @@ class MinimumCoinChange {
     int[] T = new int[change + 1];
     return coinChangeUtil(coins, change, T);
   }
+
+  /**
+   * Utility function that recursively calls itself while while calculating the minimum
+   * amount of coins requiered to give out the specified amount of change
+   * @param coins The denominations of available coins
+   * @param change The target change that should be given out
+   * @param cache Record that stores solutions to previously computed sub-problems
+   * @return The minimum number of coins required to give out the specified change
+   */
+  private static int coinChangeUtil(int[] coins, int change, int[]cache) {
+    if(change == 0) {
+        return 0;
+    }
+    
+    if(cache[change] > 0 || cache[change] == -1) {
+        return cache[change];
+    }
+    
+    int min_coins = Integer.MAX_VALUE;
+     
+    for(int i=0; i < coins.length; i++) {
+      if(coins[i] <= change) {
+        int num_coins = coinChangeUtil(coins, change - coins[i], cache);
+        if(num_coins >= 0 && num_coins < min_coins) {
+          min_coins = num_coins + 1;
+        }
+      }
+    }
+     
+    min_coins = (min_coins == Integer.MAX_VALUE) ? -1 : min_coins;
+    cache[change] = min_coins;
+     
+    return cache[change];
+  }
 }
