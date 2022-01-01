3ce2c16deb3f1e94aabd0f4873e1df753552b780
Luke Olali <mugapedia@gmail.com>
2022-01-01 22:24:05 +0300

    tests out the brute force approach for solving the rod cutting problem

--- dynamic_programming/rod_cutting.java
+++ dynamic_programming/rod_cutting.java
@@ -2,7 +2,16 @@ package dynamic_programming;
 
 class RodCutting {
   public static void main(String[] args) {
-    
+    int[] price = {0, 1, 5, 8, 9, 10, 17, 17, 20};
+		int rod_len1 = 8;
+		
+		int[] price2 = {0, 3, 5, 8, 9, 10, 17, 17, 20};
+    int rod_len2 = 8;
+
+    System.out.println("Brute Force");
+		System.out.println(cutRodBF(price, rod_len1));
+		System.out.println(cutRodBF(price2, rod_len2));
+		System.out.println("\r\n");
   }
 
   /**
