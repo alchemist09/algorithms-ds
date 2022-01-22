803dc957a1c2bc0a0433522dde5834fb13247dc8
Luke Olali <mugapedia@gmail.com>
2022-01-22 23:54:01 +0300

    tests out the brute force solution for the knapsack problem

--- dynamic_programming/knapsack.java
+++ dynamic_programming/knapsack.java
@@ -2,7 +2,22 @@ package dynamic_programming;
 
 class Knapsack {
   public static void main(String[] args) {
-    
+    int[] weights01 = {2, 3, 5, 7, 10};
+		int[] values01 = {15, 5, 6, 11, 4};
+		int capacity = 15;
+		
+		int[] weights02 = {2, 3, 5, 7};
+		int[] values02 = {16, 5, 9, 6};
+		int capacity2 = 9;
+		
+		int[] weights03 = {2, 3, 4, 5};
+    int[] values03  = {3, 4, 5, 6};
+    int capacity3 = 9;
+
+    System.out.println("Brute Force:");
+		System.out.println(knapSackBF(capacity, weights01, values01, weights01.length));
+		System.out.println(knapSackBF(capacity2, weights02, values02, weights02.length));
+		System.out.println(knapSackBF(capacity3, weights03, values03, weights03.length));
   }
 
   public static int knapSackBF(int capacity, int[] weights, int[] values, int n) {
